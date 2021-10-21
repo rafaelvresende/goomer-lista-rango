19f8ea536b07cf65521f4b960c9e180cd860659e
Rafael <rafaelvresende@gmail.com>
2021-10-21 17:45:14 -0300

    adding database password configuration to env

--- scripts/createDatabase.ts
+++ scripts/createDatabase.ts
@@ -6,6 +6,7 @@ try {
     const con = mysql.createConnection({
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
         multipleStatements: true,
     });
 
